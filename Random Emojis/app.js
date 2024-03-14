2fbb8b65b21cd4eef03f2ac3329149388e18074f
Victor <93828164+VICTOROZOR@users.noreply.github.com>
2024-03-14 23:36:03 +0100

    Add files via upload

---
+++ Random Emojis/app.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = [
+  "😁",
+  "😆",
+  "😅",
+  "🤣",
+  "😂",
+  "😀",
+  "😃",
+  "😄",
+  "😁",
+  "😆",
+  "😅",
+  "😍",
+  "😇",
+  "😊",
+  "😉",
+  "🙂",
+  "😂",
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
